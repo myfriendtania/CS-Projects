60299c5fdfa81156f857595b99ac028fe2e329de
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-05-04 14:46:13 -0500

    PowerOfTwo.cs
    This is an assignment used to find the POW of two. I struggled with this assignment heavily because mathematics and certain factorial functions are foreign to me still.

---
+++ PowTest_TaniaTorres.cs
@@ -0,0 +1,63 @@
+﻿using System;
+    class PowTest
+    {
+  
+        static void Main()
+        {
+
+            //displaying header
+
+            Console.WriteLine("Number          Power of Two");
+            Console.WriteLine("============================");
+
+            //loops 11 times
+
+            for (int i = 0; i <= 10; i++)
+            {
+
+                //prints formatted output
+
+                Console.WriteLine($"{i}                 {Pow(i)}");
+
+            }
+
+        }
+
+        static long Pow(long number)
+        {
+
+            //base cases
+
+            if (number == 1)
+            {
+
+                return 2;
+
+            }
+
+            //second base case
+
+            else if (number == 0)
+            {
+
+                return 1;
+
+            }
+
+            else
+            {
+
+                //calling recursivly
+
+                return 2 * Pow(number - 1);
+
+            }
+
+        }
+
+    }
+
+}
+
+
+ 
