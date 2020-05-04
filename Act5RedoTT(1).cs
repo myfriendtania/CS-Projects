fbca589670fa1720ae6699c60f26c36684300951
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-05-04 14:47:43 -0500

    Activity5.cs

---
+++ Act5RedoTT(1).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Act5redo
+{
+
+    class Factorial
+    {
+        static void Main()
+        {
+            Console.WriteLine("Number           Factorial");
+            Console.WriteLine("--------------------------");
+
+            for (long counter = 0; counter <= 10; ++counter)
+            {
+                Console.WriteLine($"{counter}! = {Factorial(counter)}");
+            }
+        }
+
+        static long Factorial(long number) {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+}
