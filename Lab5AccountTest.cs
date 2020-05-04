247d94e0b5a90cf19023872545f5730e781ea189
Tania <20519161+thetalentedleo@users.noreply.github.com>
2020-05-04 13:35:16 -0500

    DeckOfCards.cs

---
+++ Lab5AccountTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Activity3TaniaTorresLab5
+{
+    class AccountTest
+    {
+
+        public static void Main()
+        {
+
+            double principle;
+            double rate;
+            double time;
+            string choice = "1";
+
+
+            while (choice != "-1")
+
+            {
+                Console.Write("Please enter the initial balance of your account : \n");
+                principle = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.Write("Please enter the annual interest rate :\n");
+                rate = Convert.ToDouble(Console.ReadLine());
+
+
+                Console.Write("How many years will you accrue interest? \n");
+                time = Convert.ToDouble(Console.ReadLine());
+
+
+                Account A = new Account(principle, rate, time);
+
+                for (int i = 0; i < 12 * time; i++)
+                {
+                    principle += (principle * A.addMonthlyInterest());
+                }
+
+                Console.Write("Your balance after " + time + " years is $" + principle + "\n");
+                Console.Write("Press any key to continue or -1 for exit......\n\n");
+
+                choice = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Lab5_TaniaTorres.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LabFiveTaniaTorres
+{
+
+    class Account
+
+    {
+        private double principle;
+        private double rate;
+        private double time;
+
+        public Account(double principle, double rate, double time)
+        {
+
+
+            this.principle = principle;
+            this.rate = rate;
+            this.time = time;
+        }
+        public double addMonthlyInterest()
+        {
+            return (rate / 100) / 12;
+        }
+    }
+}
+
